9c14c9135b43c0a4b5d7bb05c7f0561f68d9c3b8
Felk <studio.felk@gmail.com>
2015-02-04 01:03:06 +0100

    starting internal server on client start (for now)

--- Game++6/src/game6/client/main/Main.java
+++ Game++6/src/game6/client/main/Main.java
@@ -1,11 +1,20 @@
 package game6.client.main;
 
+import game6.server.Server;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+		// Start internal server
+		Thread t = new Thread(() -> {
+			new Server(34543);
+		});
+		t.setDaemon(true);
+		t.start();
+
 		new Game().start();
-		
+
 	}
-	
+
 }
--- Game++6/src/game6/server/world/World.java
+++ Game++6/src/game6/server/world/World.java
@@ -20,7 +20,6 @@ public class World {
 	}
 
 	public void update() {
-		System.out.println("World update.");
 	}
 
 	public Map getMap() {
