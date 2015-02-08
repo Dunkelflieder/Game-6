5d1e637e4b8f6f72318990b194ce99377643560c
Nerogar <nerogar@arcor.de>
2015-02-08 01:02:56 +0100

    changed building image rendering

--- Game++6/src/game6/client/buildings/BuildingImageRenderer.java
+++ Game++6/src/game6/client/buildings/BuildingImageRenderer.java
@@ -1,5 +1,6 @@
 package game6.client.buildings;
 
+import game6.core.buildings.CoreBuilding;
 import de.nerogar.render.*;
 
 public class BuildingImageRenderer {
@@ -8,18 +9,23 @@ public class BuildingImageRenderer {
 	private static ScreenProperties screenProperties;
 	private static Camera guiTextureCam;
 
-	public static Texture2D render(GameDisplay display, BaseBuilding building) {
+	public static Texture2D render(GameDisplay display, CoreBuilding building) {
 		guiTextureCam = new Camera();
 		guiTextureCam.x = building.getSizeX() / 2f;
+		guiTextureCam.y = 0.5f;
+		guiTextureCam.z = (float)building.getSizeY() + 2;
+		guiTextureCam.pitch = 0;
+		
+		/*guiTextureCam.x = building.getSizeX() / 2f;
 		guiTextureCam.y = 1.0f;
 		guiTextureCam.z = building.getSizeY() + 0.5f;
-		guiTextureCam.pitch = 30;
+		guiTextureCam.pitch = 30;*/
 
 		renderTarget = new RenderTarget(true, new Texture2D("color", 0, 0));
 
 		screenProperties = new ScreenProperties();
 		screenProperties.setCamera(guiTextureCam);
-		screenProperties.setFov(90);
+		screenProperties.setFov(50);
 		screenProperties.setScreenDimension(128, 128);
 		screenProperties.setClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 		screenProperties.setRenderTarget(renderTarget);
@@ -28,12 +34,10 @@ public class BuildingImageRenderer {
 
 		building.render();
 
-		System.out.println("abc: " + building.getPosX());
-
 		Texture2D buildingImage = renderTarget.getTexture("color");
 		renderTarget.removeTexture("color");
 		renderTarget.cleanup();
-
+		
 		return buildingImage;
 	}
 
