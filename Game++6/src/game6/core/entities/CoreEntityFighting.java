6b2b09d6632c0ca5abac7230a04d8ce774f5deb2
Felk <studio.felk@gmail.com>
2015-02-15 19:09:43 +0100

    fixed inheritance

--- Game++6/src/game6/core/entities/CoreEntityFighting.java
+++ Game++6/src/game6/core/entities/CoreEntityFighting.java
@@ -1,16 +1,15 @@
 package game6.core.entities;
 
-import de.nerogar.engine.entity.BaseEntity;
 import de.nerogar.physics.BoundingAABB;
 import de.nerogar.util.Vector3f;
 import de.nerogar.util.Vectorf;
 
-public abstract class CoreEntityFighting extends BaseEntity{
+public abstract class CoreEntityFighting extends CoreEntity {
 
 	protected Vector3f rotation;
 
-	public CoreEntityFighting(BoundingAABB bounding, Vectorf<?> position) {
-		super(bounding, position);
+	public CoreEntityFighting(long id, BoundingAABB bounding, Vectorf<?> position) {
+		super(id, bounding, position);
 	}
 
 }
